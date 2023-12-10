315a6d881357e8c7ea2da1607885954c65feb3db
naoise <G00385498@atu.ie>
2023-12-10 22:37:08 +0000

    Made changes to Person class adding private variables, constructors, getters and setters

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,4 +1,48 @@
 package ie.atu;
+import java.util.Scanner;
 
 public class Person {
+    private String name;
+    private String address;
+    private String phone;
+
+    public Person(){
+
+    }
+
+    public Person(String name,String address,String phone){
+        this.name=name;
+        this.address=address;
+        this.phone=phone;
+    }
+
+    public void setPersonName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Name: ");
+        this.name=scanner.nextLine();
+    }
+
+    public String getPersonName(){
+        return name;
+    }
+
+    public void setPersonAddress(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter address: ");
+        this.address=scanner.nextLine();
+    }
+
+    public String getPersonAddress(){
+        return address;
+    }
+
+    public void setPersonPhone(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Phone Number: ");
+        this.phone=scanner.nextLine();
+    }
+
+    public String getPersonPhone(){
+        return phone;
+    }
 }
