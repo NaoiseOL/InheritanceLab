bd53dbd2338ebe8138838d0684cdf514aec460f4
naoise <G00385498@atu.ie>
2023-12-10 23:09:30 +0000

    Printed out Customer information using to String method

--- src/main/java/ie/atu/Customer.java
+++ src/main/java/ie/atu/Customer.java
@@ -1,27 +1,43 @@
 package ie.atu;
 
-public class Customer extends Person{
+import java.util.Scanner;
+
+public class Customer extends Person {
     private String customerNumber;
     private boolean mailingList;
 
-    public Customer(String name,String address,String phone, String customerNumber,boolean mailinglist){
-        super(name,address,phone);
+    public Customer() {
         this.customerNumber = "";
         this.mailingList = true;
     }
 
-    public void setCustomerNumber(String customerNumber){
+    public Customer(String name, String address, String phone, String customerNumber, boolean mailinglist) {
+        super(name, address, phone);
+        this.customerNumber = customerNumber;
+        this.mailingList = mailinglist;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
         this.customerNumber = customerNumber;
     }
-    public String getCustomerNumber(){
+
+    public String getCustomerNumber() {
         return customerNumber;
     }
 
-    public void setMailingList(boolean mailingList){
-        this.mailingList=mailingList;
+    public void setMailingList(boolean mailingList) {
+        this.mailingList = mailingList;
     }
 
     public boolean isMailingList() {
         return mailingList;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "customerNumber='" + customerNumber + '\'' +
+                ", mailingList=" + mailingList
+                ;
     }
 }
